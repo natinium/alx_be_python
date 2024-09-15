e7a265be4f24b8c0abcbc2f3aa701577f8337aea
Natnael Mesele <nmesele5@gmail.com>
2024-09-15 20:27:25 +0300

    Fix the variable name error in the finance calculator script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expense = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthly_savings = monthly_income - total_monthly_expense
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are $" + str(monthly_savings) + ".")
