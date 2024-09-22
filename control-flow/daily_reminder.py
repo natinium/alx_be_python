9b5b3c783f879293191cbea1f331b6d6bcc971ad
Natnael Mesele <nmesele5@gmail.com>
2024-09-22 18:30:55 +0300

    Fix some errors in daily reminder script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ")
-priority = input("What is the task's priority? (high/medium/low) ")
-time_bound = input("Is it time-bound? yes/no ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
