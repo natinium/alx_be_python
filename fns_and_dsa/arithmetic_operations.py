f5ba2083baca2a3f2e748cdba4f9e89b57f2989d
Natnael Mesele <nmesele5@gmail.com>
2024-09-29 13:37:13 +0300

    create and add the arthimetic operations module

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1 : float, num2 : float, operation : str):
+    match operation:
+        case "addi":
+            result = num1 + num2
+            return result
+        case "subtract":
+            result = num1 - num2
+            return result
+        case "mutltiply":
+            result = num1 * num2
+            return result
+        case "divide":
+            result = "You can't divede with zero." if nuum2 == 0 else num1 / num2
+            return result
