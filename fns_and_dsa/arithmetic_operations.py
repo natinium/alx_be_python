9001f3af4dee9405c95dbbe28d8f1bde307a37c0
Natnael Mesele <nmesele5@gmail.com>
2024-09-29 15:29:27 +0300

    rewrite the perform operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,13 @@
-def perform_operation(num1 , num2, operation):
-    match operation:
-        case "add":
-            result = num1 + num2
-            return result
-        case "subtract":
-            result = num1 - num2
-            return result
-        case "multiply":
-            result = num1 * num2
-            return result
-        case "divide":
-            result = "You can't divede with zero." if num2 == 0 else num1 / num2
-            return result
-        case _:
-            return "Error: Invalid operation"
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
